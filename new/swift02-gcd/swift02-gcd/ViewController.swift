5a24020cf44cf8e4cc786ad06e74f5d0f540e69d
jyz <jyz@jyzdeiMac.local>
2017-12-17 15:06:29 +0800

    通过闭包回调传递参数

--- new/swift02-gcd/swift02-gcd/ViewController.swift
+++ new/swift02-gcd/swift02-gcd/ViewController.swift
@@ -13,22 +13,47 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        loadDate()
+        loadDate{ (ressult) in
+            print("获取的新闻数据\(ressult)")
+        }
     }
-    func  loadDate() -> (){
+    /**  
+     
+     
+     
+     
+     
+     
+     
+     
+     */
+    
+    
+    
+    
+    
+    
+    func  loadDate(completion: @escaping ( _ result: [String]) -> ()) ->(){
         
         
         //翻译：
         //将任务添加到队列，指定执行任务的函数
         DispatchQueue.global().async {
             print("好事操作\(Thread.current)")
-        
+            
+            //休眠
+            Thread.sleep(forTimeInterval: 1.0 )
+            
+            
+            //获取结果
+             let json = ["头条","八卦","出大事啦"]
         //主队列回调
             DispatchQueue.main.async(execute:{
                 print("主线程更新UI \(Thread.current)")
+                completion(json)
             })
                 
-            } 
+            }
         
         
     
